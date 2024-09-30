7213c63976c3cc108b14495ec4c4420a77dfdafa
José Barrera <josebarrerag1996@gmail.com>
2024-09-30 09:34:45 -0300

    Crear sesión de pago

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
 import Stripe from 'stripe';
+import { envs } from 'src/config';
 
 @Injectable()
 export class PaymentsService {
   // Instanciando a Stripe
   private readonly stripe = new Stripe(envs.stripeSecretKey);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {},
+      },
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-Shirt',
+            },
+            unit_amount: 2000, // 20 dólares
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancel',
+    });
+    return session;
+  }
 }
