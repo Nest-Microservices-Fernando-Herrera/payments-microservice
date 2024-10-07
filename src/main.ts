383b2b870c2f33cf896c4b9aad73edba67ba7ea4
José Barrera <josebarrerag1996@gmail.com>
2024-10-07 09:15:41 -0300

    Payment Session DTO

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('PaymentsMS-Main');
 
   const app = await NestFactory.create(AppModule);
+
+  // Configuración global de los pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Validando el uso de los DTOs
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(envs.port);
 
   logger.log(`Payments Microservice running on port ${envs.port}`);
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { envs } from 'src/config';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
   // Instanciando a Stripe
   private readonly stripe = new Stripe(envs.stripeSecretKey);
 
-  async createPaymentSession() {
+  // Método para crear una sesión de pago en Stripe
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    // Desestructurando el DTO
+    const { currency, items, orderId } = paymentSessionDto;
+
+    // Creamos un array de objetos lineItems para Stripe
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          // Convertimos el precio a centavos para Stripe (multiplicando por 100)
+          unit_amount: Math.round(item.price * 100), // Ejemplo -> 20 dólares 2000 / 100 = 20.00 // 15.0000
+        },
+        quantity: item.quantity,
+      };
+    });
+
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
-        metadata: {},
-      },
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-Shirt',
-            },
-            unit_amount: 2000, // 20 dólares
-          },
-          quantity: 2,
+        metadata: {
+          orderId: orderId,
         },
-      ],
+      },
+      line_items: lineItems,
       mode: 'payment',
       success_url: 'http://localhost:3003/payments/success',
       cancel_url: 'http://localhost:3003/payments/cancel',
